0d867a346564df133eaf7c1d75a5781195a7ef4b
bhabna01 <abierhoque01@gmail.com>
2022-11-09 21:35:28 +0600

    15th commit

--- src/Pages/ServicesDetails/ServiceDetails.js
+++ src/Pages/ServicesDetails/ServiceDetails.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Review from '../../Review/Review';
 
 const ServiceDetails = () => {
-    const { _id, title, img, description, price, rating } = useLoaderData();
+    const service = useLoaderData();
+    const { _id, price, rating, title, description, img } = service;
+
+
     return (
-        <div className="card lg:card-side bg-base-100 shadow-xl">
-            <figure><img src={img} alt="Album" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">{title}</h2>
-                <p>{description}</p>
-                <p>Price:{price}</p>
-                <p>Rating:{rating}</p>
+        <div>
+            <div className="card lg:card-side bg-base-100 shadow-xl">
+                <figure><img src={img} alt="Album" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{title}</h2>
+                    <p>{description}</p>
+                    <p>Price:{price}</p>
+                    <p>Rating:{rating}</p>
+
+                </div>
+            </div>
+
+            <div className='mt-20'>
+                <Review service={service} key={_id}></Review>
             </div>
         </div>
+
     );
 };
 
---
+++ src/Review/Review.js
@@ -0,0 +1,48 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import { AuthContext } from "../context/AuthProvider/AuthProvider";
+import ReviewForm from "../Pages/ReviewForm/ReviewForm";
+
+
+import SingleReview from "../SingleReview/SingleReview";
+
+const Review = ({ service }) => {
+    const { user } = useContext(AuthContext);
+    const [reviews, setReviews] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:5000/reviews?service=${service._id}`)
+            .then((response) => response.json())
+            .then((data) => setReviews(data));
+    }, [reviews, service._id]);
+
+
+    return (
+        <div>
+            <h3 className="text-warning">
+                Customer Reviews for {service.title}
+
+            </h3>
+            <p className="text-success">No of Reviews:{reviews.length}</p>
+            <div className="">
+                {reviews.length === 0 ? <h1 className="text-danger text-center">No Reviews Found</h1> : <>{reviews.map((review) => (
+                    <SingleReview review={review} key={review._id}></SingleReview>
+                ))}</>}
+
+
+            </div>
+            {user ? (
+                <ReviewForm service={service}></ReviewForm>
+            ) : (
+                <>
+                    <div className="d-flex">
+                        <h3>
+                            Please login to add a review <Link to="/login">Login</Link>
+                        </h3>
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Review;
